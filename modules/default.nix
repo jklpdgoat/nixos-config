08d7b1d50e849dfdca96bc0f031b6f90848c8039
jklpdgoat <jarpadul@gmail.com>
2022-12-19 10:58:47 +0800

    Re-enable foot terminal

--- modules/default.nix
+++ modules/default.nix
@@ -5,7 +5,7 @@
     imports = [
         # gui
         #./sway
-        #./foot
+        ./foot
         #./wofi
 
         ## cli
--- modules/foot/default.nix
+++ modules/foot/default.nix
@@ -11,8 +11,9 @@ in {
       settings = {
         main = {
           #font = "JetBrainsMono Nerdfont:size=7:line-height=16px";
-          font = "Hack Nerd Font Mono:size=8.50";
-          #dpi-aware = "yes";
+          #font = "Hack Nerd Font Mono:size=8.50";
+          font = "JetBrainsMono Nerd Font:size=8.50";
+          dpi-aware = "yes";
           #pad = "12x12";
         };
         
@@ -27,26 +28,6 @@ in {
 
         colors = {
           alpha = "0.80";
-        #    foreground = "d9e0ee";
-        #    background = "292a37";
-        #    ## Normal/regular colors (color palette 0-7)
-        #    regular0="303241";  # black
-        #    regular1="ec6a88";
-        #    regular2="3fdaa4";
-        #    regular3="efb993";
-        #    regular4="3fc6de";
-        #    regular5="b771dc";
-        #    regular6="6be6e6";
-        #    regular7="d9e0ee";
-
-        #    bright0="393a4d"; # bright black
-        #    bright1="e95678"; # bright red
-        #    bright2="29d398";# bright green
-        #    bright3="efb993";# bright yellow
-        #    bright4="26bbd9";
-        #    bright5="b072d1";# bright magenta
-        #    bright6="59e3e3";# bright cyan
-        #    bright7="d9e0ee";# bright white
         };
       };
     };
